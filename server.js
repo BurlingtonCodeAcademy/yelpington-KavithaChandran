243cfc83f79867b11d0a248a4e40674b63485f02
Kavitha <c.kavitha1987@gmail.com>
2021-05-07 12:20:35 -0400

    start changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const staticDir = path.resolve("./client/build");
 // set up port variable / server  to host at 5000 as default
 const port = process.env.PORT || 5000
 
